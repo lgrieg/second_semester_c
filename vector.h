b8628d1266a891a07119d58ebe71b0b46aff1059
lgrieg <79267710+lgrieg@users.noreply.github.com>
2021-10-26 14:06:03 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -12,222 +12,198 @@ public:
 
 template <class T>
 class Vector {
+
+    size_t vector_size_;
+    size_t capacity_;
+    T* buffer_;
+
 public:
-    T* buffer;
-    size_t vector_size;
-    size_t capacity;
+    Vector() : vector_size_(0), capacity_(0), buffer_(nullptr){};
 
-    Vector() {
-        buffer = nullptr;
-        vector_size = 0;
-        capacity = 0;
-    }
+    explicit Vector(size_t size) : vector_size_(size), capacity_(size), buffer_(new T[size]){};
 
-    explicit Vector(size_t size) {
-        buffer = new T[size];
-        capacity = size;
-        vector_size = size;
-    }
-
-    Vector(size_t size, const T& value) {
-        buffer = new T[size];
-        for (size_t i = 0; i < size; ++i) {
-            buffer[i] = value;
+    void FillBuffer(size_t start_id, size_t end_id, const T& value) {
+        for (size_t i = start_id; i < end_id; ++i) {
+            buffer_[i] = value;
         }
-        vector_size = size;
-        capacity = size;
     }
 
-    Vector(const Vector& other) : vector_size(other.vector_size), capacity(other.capacity) {
-        if (capacity == 0) {
-            buffer = nullptr;
-        } else {
-            buffer = new T[capacity];
-            for (size_t i = 0; i < vector_size; ++i) {
-                buffer[i] = other.buffer[i];
-            }
+    Vector(size_t size, const T& value) : Vector(size) {
+        // buffer_ = new T[size];
+        FillBuffer(0, size, value);
+        // vector_size_ = size;
+        // capacity_ = size;
+    }
+
+    void Copy(const Vector& other) {
+        other.CopyTo(buffer_);
+    }
+
+    void CopyTo(T* new_buffer) const {
+        for (size_t i = 0; i < vector_size_; ++i) {
+            new_buffer[i] = buffer_[i];
         }
+    }
+
+    void ReplaceBuffer(T* new_buffer) {
+        delete[] buffer_;
+        buffer_ = new_buffer;
+    }
+
+    void CreateBufferCopyReplaceUpdCapacity(size_t new_capacity) {
+        T* new_buffer = new T[new_capacity];
+        CopyTo(new_buffer);
+        ReplaceBuffer(new_buffer);
+        capacity_ = new_capacity;
+    }
+
+    Vector(const Vector& other)
+        : vector_size_(other.vector_size_)
+        , capacity_(other.capacity_)
+        , buffer_(capacity_ == 0 ? nullptr : new T[capacity_]) {
+        Copy(other);
     }
 
     Vector& operator=(const Vector& other) {
         if (this != &other) {
-            T* new_buffer = new T[other.capacity];
-            for (size_t i = 0; i < other.vector_size; ++i) {
-                new_buffer[i] = other.buffer[i];
+            if (capacity_ < other.capacity_) {
+                T* new_buffer = new T[other.capacity_];
+                ReplaceBuffer(new_buffer);
             }
-
-            if (buffer != nullptr) {
-                delete[] buffer;
-            }
-            buffer = new_buffer;
-            vector_size = other.vector_size;
-            capacity = other.capacity;
+            Copy(other);
+            vector_size_ = other.vector_size_;
+            capacity_ = other.capacity_;
         }
         return *this;
     }
 
     void Clear() {
-        vector_size = 0;
+        vector_size_ = 0;
     }
 
     void Resize(size_t new_size) {
-        if (new_size > capacity) {
-            T* new_buffer = new T[new_size];
-            for (size_t i = 0; i < vector_size; ++i) {
-                new_buffer[i] = buffer[i];
-            }
-
-            if (buffer != nullptr) {
-                delete[] buffer;
-            }
-            buffer = new_buffer;
-            capacity = new_size;
+        if (new_size > capacity_) {
+            CreateBufferCopyReplaceUpdCapacity(new_size);
         }
-        vector_size = new_size;
+        vector_size_ = new_size;
     }
 
     void Resize(size_t new_size, T value) {
-        if (new_size > capacity) {
-            std::cout << "realloc" << std::endl;
-            T* new_buffer = new T[new_size];
-            for (size_t i = 0; i < vector_size; ++i) {
-                new_buffer[i] = buffer[i];
-            }
-
-            if (buffer != nullptr) {
-                delete[] buffer;
-            }
-            buffer = new_buffer;
-            capacity = new_size;
+        const size_t old_vector_size = vector_size_;
+        Resize(new_size);
+        if (new_size > old_vector_size) {
+            FillBuffer(old_vector_size, new_size, value);
         }
-        if (new_size > vector_size) {
-            for (size_t j = vector_size; j < new_size; ++j) {
-                buffer[j] = value;
-            }
-        }
-        vector_size = new_size;
     }
 
     void PushBack(const T& value) {
-        if (capacity == 0) {
-            size_t old_size = vector_size;
-            Resize(1);
-            vector_size = old_size;
-        } else if (vector_size == capacity) {
-            size_t old_size = vector_size;
-            Resize(capacity * 2);
-            vector_size = old_size;
+        if (capacity_ == 0) {
+            Reserve(1);
+            vector_size_ = 0;
+        } else if (vector_size_ == capacity_) {
+            const size_t old_size = vector_size_;
+            Reserve(capacity_ * 2);
+            vector_size_ = old_size;
         }
-        buffer[vector_size] = value;
-        ++vector_size;
+        buffer_[vector_size_] = value;
+        ++vector_size_;
     }
 
     void PopBack() {
-        --vector_size;
+        --vector_size_;
     }
 
-    void Reserve(const size_t& new_cap) {
-        if (new_cap > capacity) {
-            T* new_buffer = new T[new_cap];
-            for (size_t i = 0; i < vector_size; ++i) {
-                new_buffer[i] = buffer[i];
-            }
-            if (buffer != nullptr) {
-                delete[] buffer;
-            }
-            buffer = new_buffer;
-            capacity = new_cap;
+    void Reserve(size_t new_cap) {
+        if (new_cap > capacity_) {
+            CreateBufferCopyReplaceUpdCapacity(new_cap);
         }
     }
 
     void ShrinkToFit() {
-        if (capacity > vector_size) {
-            T* new_buffer = new T[vector_size];
-            for (size_t i = 0; i < vector_size; ++i) {
-                new_buffer[i] = buffer[i];
-            }
-            if (buffer != nullptr) {
-                delete[] buffer;
-            }
-            buffer = new_buffer;
-            capacity = vector_size;
+        if (capacity_ > vector_size_) {
+            CreateBufferCopyReplaceUpdCapacity(vector_size_);
         }
     }
 
     void Swap(Vector& other) {
-        std::swap(vector_size, other.vector_size);
-        std::swap(capacity, other.capacity);
-        std::swap(buffer, other.buffer);
+        std::swap(vector_size_, other.vector_size_);
+        std::swap(capacity_, other.capacity_);
+        std::swap(buffer_, other.buffer_);
     }
 
-    T operator[](const size_t& index) const {
-        return buffer[index];
+    T operator[](size_t index) const {
+        return buffer_[index];
     }
 
-    T& operator[](const size_t& index) {
-        return buffer[index];
+    T& operator[](size_t index) {
+        return buffer_[index];
     }
 
-    const T At(const size_t& index) const {
-        if (index < 0 || index >= vector_size) {
+    const T At(size_t index) const {
+        if (index >= vector_size_) {
             throw VectorOutOfRange{};
         }
-        return buffer[index];
+        return buffer_[index];
     }
 
-    T& At(const size_t& index) {
-        if (index < 0 || index >= vector_size) {
+    T& At(size_t index) {
+        if (index >= vector_size_) {
             throw VectorOutOfRange{};
         }
-        return buffer[index];
+        return buffer_[index];
     }
 
     T Front() const {
-        return buffer[0];
+        return buffer_[0];
     }
 
     T& Front() {
-        return buffer[0];
+        return buffer_[0];
     }
 
     T Back() const {
-        return buffer[vector_size - 1];
+        return buffer_[vector_size_ - 1];
     }
 
     T& Back() {
-        return buffer[vector_size - 1];
+        return buffer_[vector_size_ - 1];
     }
 
     bool Empty() const {
-        return vector_size == 0;
+        return vector_size_ == 0;
     }
 
     size_t Size() const {
-        return vector_size;
+        return vector_size_;
     }
 
     size_t Capacity() const {
-        return capacity;
+        return capacity_;
     }
 
     T* Data() {
-        return buffer;
+        return buffer_;
+    }
+
+    const T* Data() const {
+        return buffer_;
     }
 
     ~Vector() {
-        if (buffer != nullptr) {
-            delete[] buffer;
-        }
+        delete[] buffer_;
     }
 };
 
 template <class T>
 bool operator<(const Vector<T>& a, const Vector<T>& b) {
-    for (size_t i = 0; i < std::min(a.Size(), b.Size()); ++i) {
-        if (a.buffer[i] == b.buffer[i]) {
-            continue;
+    const size_t min_size = std::min(a.Size(), b.Size());
+    for (size_t i = 0; i < min_size; ++i) {
+        if (a[i] < b[i]) {
+            return true;
         }
-        return a.buffer[i] < b.buffer[i];
+        if (a[i] > b[i]) {
+            return false;
+        }
     }
     return a.Size() < b.Size();
 }
@@ -249,7 +225,16 @@ bool operator<=(const Vector<T>& a, const Vector<T>& b) {
 
 template <class T>
 bool operator==(const Vector<T>& a, const Vector<T>& b) {
-    return (b <= a) && (b >= a);
+    const size_t a_size = a.Size();
+    const size_t b_size = b.Size();
+    if (a_size == b_size) {
+        for (size_t i = 0; i < a_size; ++i) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+    }
+    return a_size == b_size;
 }
 
 template <class T>
